9848459179e967aa432073cdb79ad57c30077594
Pankesh <post4pankesh@gmail.com>
2017-07-14 19:43:38 +0530

    adding deletion methods for linked lists

--- linked_list.py
+++ linked_list.py
@@ -23,7 +23,15 @@ class linked_list:
             length+=1
             current = current.get_next_node()
         return length
-            
+    def return_node(self,pos):  
+        current = self.get_head()
+        node_demand = None
+        current_pos = 0
+        while pos >=0 and pos <= self.length() and current_pos != pos:
+            node_demand = current.get_next_node()
+            current_pos +=1
+        return node_demand
+    # insertion method's ##########################        
     def insetion_node_begin(self,data):
         a_node = node(data)
         a_node.next_node = self.get_head()
@@ -34,15 +42,7 @@ class linked_list:
         a_node = node(data)
         last_node = self.get_end()
         last_node.next_node = a_node
-        print("inseted a new node at the end")
-    def return_node(self,pos):
-        current = self.get_head()
-        node_demand = None
-        current_pos = 0
-        while pos >=0 and pos <= self.length() and current_pos != pos:
-            node_demand = current.get_next_node()
-            current_pos +=1
-        return node_demand 
+        print("inseted a new node at the end") 
     def insertion_node_pos(self,pos,data):
         postion = None
         if pos==0 :
@@ -55,6 +55,34 @@ class linked_list:
                 m_node = self.return_node(pos)
                 a_node.set_next_node(m_node.get_next_node())
                 m_node.set_next_node(a_node)
+
+    # deletion method
+    def delete_node(self,pos):
+        if pos==0:
+            delete_node_begin(pos)
+        elif pos == self.length():
+            delete_node_end(pos)
+        elif pos>0 and pos< self.length():
+            previous_node = self.return_node(pos-1)
+            current_node = previous_node.get_next_node()
+            previous_node.set_next_node(current_node.get_next_node())
+            del current_node
+            print("node sucessfully deleted")
+        
+    
+    def delete_node_end(self):
+        previous_node = self.return_node(self.length()-1)
+        last_node = previous_node.get_next_node()
+        previous_node.set_next_node(None)
+        del last_node
+        print("last node succesfully deleted")
+    
+    def delete_node_begin(self):
+        head_node = self.get_head()
+        self.set_head(head_node.get_next_node)
+        del head_node
+        print("Head node succesfully deleted")
+    
                 
 class node:
     def __init__(self,data,next_node = None):
